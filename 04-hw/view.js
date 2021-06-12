362052adb9269b6e05f8134fb7b5963068a40609
Vyacheslav <dyaditsyn@gmail.com>
2021-06-12 21:01:17 +0300

    📖 DOC: hw04 project initial commit

---
+++ 04-hw/view.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// #1 //
+// function example(){
+//     const input = document.getElementById("max-digit-input").value;
+//     const result = document.getElementById("max-digit-res");
+//     result.innerHTML = "";
+//     let maxDigit;
+//     try{
+//         maxDigit = calcMaxDigit(input);
+//         result.classList.remove("text-danger");
+//         result.innerHTML = `The max digit is: ${maxDigit}`;
+//     }
+//     catch(error){
+//         result.classList.add("text-danger");
+//         result.innerHTML = error;
+//     }
+// }
+
