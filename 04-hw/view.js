a0ed6ab7b5dcae84d9fd668cff0de49b8802330d
Vyacheslav <dyaditsyn@gmail.com>
2021-06-13 08:33:38 +0300

    👌 IMPROVE:

--- 04-hw/view.js
+++ 04-hw/view.js
@@ -3,23 +3,23 @@
 // #1 //
 function showPairs(){
     const result = document.getElementById("task1-res");
-    result.textContent = JSON.stringify(getPairs(students));
+    result.textContent = JSON.stringify(getPairs(students), null, " ");
 }
 
 // #2 //
 function showPairsSubject(){
     const result = document.getElementById("task2-res");
-    result.textContent = JSON.stringify(getPairsThemes(pairs, themes));
+    result.textContent = JSON.stringify(getPairsThemes(pairs, themes), null, " ");
 }
 
 // #3 //
 function showStudentGrade(){
     const result = document.getElementById("task3-res");
-    result.textContent = JSON.stringify(getStudentMarks(students, marks));
+    result.textContent = JSON.stringify(getStudentMarks(students, marks), null, " ");
 }
 
 // #4 //
 function showRandomGrade(){
     const result = document.getElementById("task4-res");
-    result.textContent = JSON.stringify(getPairsRandomMarks(pairsThemes));
+    result.textContent = JSON.stringify(getPairsRandomMarks(pairsThemes), null, " ");
 }
