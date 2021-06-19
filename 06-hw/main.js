88d370e6b92d0fca9cf92faa10261748122c0cfa
Vyacheslav <dyaditsyn@gmail.com>
2021-06-19 16:43:12 +0300

    📦 NEW: Homework #06 initial commit - creating

---
+++ 06-hw/main.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const students = [{
+    name: "Tanya",
+    course: 3,
+    subjects: {
+      math: [4, 4, 3, 4],
+      algorithms: [3, 3, 3, 4, 4, 4],
+      data_science: [5, 5, 3, 4]
+    }
+  }, {
+    name: "Victor",
+    course: 4,
+    subjects: {
+      physics: [5, 5, 5, 3],
+      economics: [2, 3, 3, 3, 3, 5],
+      geometry: [5, 5, 2, 3, 5]
+    }
+  }, {
+    name: "Anton",
+    course: 2,
+    subjects: {
+      statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
+      english: [5, 3],
+      cosmology: [5, 5, 5, 5]
+    }
+  }];
+
+// #1 //
+
+
+// ------------------------------------------------------------------------------------------------------------------ //
+
+function getAverage(...numbers) {
+    const numbersArr = [...numbers];
+    const total = numbersArr.reduce( (total, number) => {
+        return total + Number(number);
+    }, 0);
+    return total / numbersArr.length;
+}
+
---
+++ 06-hw/view.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// #1 //
+// function showRandomArray(){
+//     const inputLength = document.getElementById("length-input").value;
+//     const inputNum1 = document.getElementById("first-limit-input").value;
+//     const inputNum2 = document.getElementById("second-limit-input").value;
+//     const result = document.getElementById("random-array-res");
+//     result.textContent = "";
+//     let randomArray;
+//     try{
+//         randomArray = getRandomArray(inputLength, inputNum1, inputNum2 );
+//         result.classList.remove("text-danger");
+//         result.textContent = `Random numbers array: ${JSON.stringify(randomArray, null, " ")}`;
+//     }
+//     catch(error){
+//         result.classList.add("text-danger");
+//         result.innerHTML = error;
+//     }
+// }
+
