53c9abe3d684ff85cb2fe206d39b87f8993db5cc
Vyacheslav <dyaditsyn@gmail.com>
2021-06-22 09:53:44 +0300

    HW07 creation initial commit

---
+++ 07-hw/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+// #1 // example
+// const getSubjects = (studentIndex) => {
+//     if ( isNaN(studentIndex) ) {
+//         throw new Error("Please select a student from the list to see subjects!");
+//     }
+//     return Object.keys(students[studentIndex].subjects).map( subject => 
+//             capitalizeName(subject).replace("_", " ") );
+// }
+
+                        
+
+
+
+// ------------------------------------------------------------------------------------------------------------------ //
+
+
---
+++ 07-hw/view.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// #1 // example
+// function showSubjects(){
+//     const student = document.getElementById("subjects").value;
+//     const result = document.getElementById("subjects-res");
+//     result.textContent = "";
+//     let subjects;
+//     try{
+//         subjects = getSubjects(student);
+//         result.classList.remove("text-danger");
+//         result.textContent = `${students[student].name}'s subjects: ${JSON.stringify(subjects, null, " ")}`;
+//     }
+//     catch(error){
+//         result.classList.add("text-danger");
+//         result.innerHTML = error;
+//     }
+// }
+
+// // #2 //
+// function showMark(){
+//     const student = document.getElementById("mark").value;
+//     const result = document.getElementById("mark-res");
+//     result.textContent = "";
+//     let averageMark;
+//     try{
+//         averageMark = getAverageMark(student);
+//         result.classList.remove("text-danger");
+//         result.textContent = `${students[student].name}'s average mark is: ${averageMark}`;
+//     }
+//     catch(error){
+//         result.classList.add("text-danger");
+//         result.innerHTML = error;
+//     }
+// }
+
