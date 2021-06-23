fd2e875359bdba6fcfe46e88f097b4d1f721b261
Vyacheslav <dyaditsyn@gmail.com>
2021-06-24 01:49:10 +0300

    🚀 RELEASE: Ex7 v1.0 done

--- 07-hw/main.js
+++ 07-hw/main.js
@@ -4,19 +4,81 @@ const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
 const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
-// #1 // example
-// const getSubjects = (studentIndex) => {
-//     if ( isNaN(studentIndex) ) {
-//         throw new Error("Please select a student from the list to see subjects!");
-//     }
-//     return Object.keys(students[studentIndex].subjects).map( subject => 
-//             capitalizeName(subject).replace("_", " ") );
-// }
+// #1 //
+function getMyTaxes(inputSal) {
+    const salary = +inputNumberValid(inputSal);
+    return salary * this.tax;
+}                   
 
-                        
+// #2 //
+
+function getMiddleTaxes() {
+    return Number((this.middleSalary * this.tax).toFixed(2));
+}  
+
+// #3 // 
+
+function getTotalTaxes() {
+    return Number((this.middleSalary * this.tax * this.vacancies).toFixed(2));
+}
+
+// #4 // 
+
+function getMySalary() {
+    const salary = getRandomNumber(1500, 2000);
+    const taxes = Number((salary * this.tax).toFixed(2));
+    return {
+        salary: salary,
+        taxes: taxes,
+        profit: salary - taxes
+    };
+}
+
+const uaTimer = setInterval ( () => console.log('Ukraine', getMySalary.call(ukraine)), 10000);
+const lvTimer = setInterval ( () => console.log('Latvia', getMySalary.call(latvia)), 10000);
+const liTimer = setInterval ( () => console.log('Litva', getMySalary.call(litva)), 10000);
+
+setTimeout(() => { clearInterval(uaTimer); }, 40000);
+setTimeout(() => { clearInterval(lvTimer); }, 40000);
+setTimeout(() => { clearInterval(liTimer); }, 40000);
 
 
+
+console.log('Function 1 tests - Your taxes according to your country');
+console.log(getMyTaxes.call(ukraine, '1000'));
+console.log(getMyTaxes.call(latvia, '2000'));
+console.log(getMyTaxes.call(litva, '3000'));
+
+console.log('Function 2 tests - MIddle taxes for IT-specialists per country');
+console.log(getMiddleTaxes.call(ukraine));
+console.log(getMiddleTaxes.call(latvia));
+console.log(getMiddleTaxes.call(litva));
+
+console.log('Function 3 tests - Overall IT-specialists taxes per country');
+console.log(getTotalTaxes.call(ukraine));
+console.log(getTotalTaxes.call(latvia));
+console.log(getTotalTaxes.call(litva));
+
+console.log('Function 4 tests - profit per country ');
+console.log('Ukraine', getMySalary.call(ukraine));
+console.log('Latvia', getMySalary.call(latvia));
+console.log('Litva', getMySalary.call(litva));
 
 // ------------------------------------------------------------------------------------------------------------------ //
 
+// help function 
+function getRandomNumber(highLimit, lowLimit) {
+    return Math.floor (Math.random() * (highLimit - lowLimit + 1) ) + lowLimit;
+}
 
+// help function check input for numbers only
+function inputNumberValid(inputVal) {
+    if ( !(inputVal && inputVal.trim().length) || isNaN(Number(inputVal)) || inputVal < 0) {
+            throw new Error("Invalid input! A positive numbers only accepted");
+    }
+    return inputVal;
+}
+
+function capitalizeName(value) {
+    return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+}
--- 07-hw/view.js
+++ 07-hw/view.js
@@ -1,36 +1,93 @@
 "use strict";
 
-// #1 // example
-// function showSubjects(){
-//     const student = document.getElementById("subjects").value;
-//     const result = document.getElementById("subjects-res");
-//     result.textContent = "";
-//     let subjects;
-//     try{
-//         subjects = getSubjects(student);
-//         result.classList.remove("text-danger");
-//         result.textContent = `${students[student].name}'s subjects: ${JSON.stringify(subjects, null, " ")}`;
-//     }
-//     catch(error){
-//         result.classList.add("text-danger");
-//         result.innerHTML = error;
-//     }
-// }
+// #1 //
+function showMyTaxes() {
+    const salary = document.getElementById("salary").value;
+    const inputCountry = document.getElementById("taxes").value;
+    const result = document.getElementById("taxes-res");
+    result.textContent = "";
+    let getTax;
+    try{
+        const country = transformCountry(inputCountry);
+        getTax = getMyTaxes.call(country, salary);
+        result.classList.remove("text-danger");
+        result.textContent = `If you earn ${salary}$ in ${capitalizeName(inputCountry)}, you will pay ${getTax}$ tax`;
+    }
+    catch(error){
+        result.classList.add("text-danger");
+        result.innerHTML = error;
+    }
+}
 
-// // #2 //
-// function showMark(){
-//     const student = document.getElementById("mark").value;
-//     const result = document.getElementById("mark-res");
-//     result.textContent = "";
-//     let averageMark;
-//     try{
-//         averageMark = getAverageMark(student);
-//         result.classList.remove("text-danger");
-//         result.textContent = `${students[student].name}'s average mark is: ${averageMark}`;
-//     }
-//     catch(error){
-//         result.classList.add("text-danger");
-//         result.innerHTML = error;
-//     }
-// }
+// #2 //
+function showMiddleTaxes() {
+    const inputCountry = document.getElementById("middle").value;
+    const result = document.getElementById("middle-res");
+    result.textContent = "";
+    let middleTax;
+    try{
+        const country = transformCountry(inputCountry);
+        middleTax = getMiddleTaxes.call(country);
+        result.classList.remove("text-danger");
+        result.textContent = `In ${capitalizeName(inputCountry)}, middle tax for IT specialists is ${middleTax}$`;
+    }
+    catch(error){
+        result.classList.add("text-danger");
+        result.innerHTML = error;
+    }
+}
 
+// #3 //
+function showTotalTaxes() {
+    const inputCountry = document.getElementById("total").value;
+    const result = document.getElementById("total-res");
+    result.textContent = "";
+    let totalTax;
+    try{
+        const country = transformCountry(inputCountry);
+        totalTax = getTotalTaxes.call(country);
+        result.classList.remove("text-danger");
+        result.textContent = `In ${capitalizeName(inputCountry)}, total tax for IT specialists is ${totalTax}$`;
+    }
+    catch(error){
+        result.classList.add("text-danger");
+        result.innerHTML = error;
+    }
+}
+
+ // #4 //
+ function showProfit() {
+    const inputCountry = document.getElementById("profit").value;
+    const result = document.getElementById("profit-res");
+    result.textContent = "";
+    try{
+        const country = transformCountry(inputCountry);
+        result.classList.remove("text-danger");
+        if (country === ukraine) {
+            result.textContent = JSON.stringify(getMySalary.call(ukraine))
+            setInterval ( () => result.textContent = JSON.stringify(getMySalary.call(ukraine)), 10000);
+        }
+        else if (country === latvia) {
+            result.textContent = JSON.stringify(getMySalary.call(latvia));
+            setInterval ( () => result.textContent = JSON.stringify(getMySalary.call(latvia)), 10000);
+        }
+        else if (country === litva) {
+            result.textContent = JSON.stringify(getMySalary.call(litva));
+            setInterval ( () => result.textContent = JSON.stringify(getMySalary.call(litva)), 10000);
+        }
+    }
+    catch(error){
+        result.classList.add("text-danger");
+        result.innerHTML = error;
+    }
+}
+
+//--------------------------------------------------------------------------------------------------------------------//
+// function solve issue with country name came from input with quotes 
+function transformCountry (inputCountry) {
+    let country;
+    if (inputCountry === "ukraine") return country = ukraine;
+    else if (inputCountry === "latvia") return country = latvia;
+    else if (inputCountry === "litva") return country = litva;
+    else throw new Error("Please select the country from the list");
+}
