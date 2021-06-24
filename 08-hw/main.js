c373990d3e6230dc04cbf695d1dd41da1fc4a6ce
Vyacheslav <dyaditsyn@gmail.com>
2021-06-24 16:17:08 +0300

    📦 NEW: HW#08 creation initial commit

---
+++ 08-hw/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+
+// ------------------------------------------------------------------------------------------------------------------ //
+
+// // help function 
+// function getRandomNumber(highLimit, lowLimit) {
+//     return Math.floor (Math.random() * (highLimit - lowLimit + 1) ) + lowLimit;
+// }
+
+// // help function check input for numbers only
+// function inputNumberValid(inputVal) {
+//     if ( !(inputVal && inputVal.trim().length) || isNaN(Number(inputVal)) || inputVal < 0) {
+//             throw new Error("Invalid input! A positive numbers only accepted");
+//     }
+//     return inputVal;
+// }
+
+// function capitalizeName(value) {
+//     return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+// }
---
+++ 08-hw/view.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// #1 //
+// function showMyTaxes() {
+//     const salary = document.getElementById("salary").value;
+//     const inputCountry = document.getElementById("taxes").value;
+//     const result = document.getElementById("taxes-res");
+//     result.textContent = "";
+//     let getTax;
+//     try{
+//         const country = transformCountry(inputCountry);
+//         getTax = getMyTaxes.call(country, salary);
+//         result.classList.remove("text-danger");
+//         result.textContent = `If you earn ${salary}$ in ${capitalizeName(inputCountry)}, you will pay ${getTax}$ tax`;
+//     }
+//     catch(error){
+//         result.classList.add("text-danger");
+//         result.innerHTML = error;
+//     }
+// }
+
+//--------------------------------------------------------------------------------------------------------------------//
+// function solve issue with country name came from input with quotes 
+// function transformCountry (inputCountry) {
+//     let country;
+//     if (inputCountry === "ukraine") return country = ukraine;
+//     else if (inputCountry === "latvia") return country = latvia;
+//     else if (inputCountry === "litva") return country = litva;
+//     else throw new Error("Please select the country from the list");
+// }
