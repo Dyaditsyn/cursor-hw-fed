cd32a3dd18a038e3edff9b5d2bf29f1adef14015
Vyacheslav <dyaditsyn@gmail.com>
2021-07-13 18:43:32 +0300

    📦 NEW: added keydown events

--- 10-hw/js/main.js
+++ 10-hw/js/main.js
@@ -1,14 +1,29 @@
 "use strict";
+const WHITE_KEYS = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
+const BLACK_KEYS = ['w', 'e', 't', 'y', 'u'];
 
-let keys = document.querySelectorAll('.key');
+const keys = document.querySelectorAll('.key');
+const whiteKeys = document.querySelectorAll('.key-w');
+const blackKeys = document.querySelectorAll('.key-b');
 
 keys.forEach(key => {
-    key.addEventListener('click', playNote);
+    key.addEventListener('click', () => playNote(key));
 });
 
-function playNote(e) {
-    let key = e.target;
-    let note = document.getElementById(key.dataset.note)
+document.addEventListener('keydown', e => {
+    const key = e.key.toLowerCase();
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+    const blackKeyIndex = BLACK_KEYS.indexOf(key);
+    if(whiteKeyIndex > -1) {
+        playNote(whiteKeys[whiteKeyIndex])
+    }
+    if(blackKeyIndex > -1) {
+        playNote(blackKeys[blackKeyIndex])
+    }
+});
+
+function playNote(key) {
+    const note = document.getElementById(key.dataset.note)
     key.classList.add('active');
     note.currentTime = 0;
     note.play();
@@ -16,3 +31,6 @@ function playNote(e) {
         key.classList.remove('active');
     })
 }
+
+
+
